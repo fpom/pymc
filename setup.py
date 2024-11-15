bc75f9b0e38354d370e7a86846fe2139b73617f0
Franck Pommereau <franck.pommereau@gmail.com>
2024-11-15 17:59:17 +0100

    updated dependencies

--- setup.py
+++ setup.py
@@ -1,21 +1,27 @@
 from setuptools import setup, find_packages
-import pathlib, inspect
-import pymc
+import pathlib
 
 long_description = pathlib.Path("README.md").read_text(encoding="utf-8")
-description = inspect.cleandoc(pymc.__doc__).splitlines()[0]
+description = "libDDD-based model-checker for (AR)CTL with(out) fairness"
 
-setup(name="pymc",
-      description=description,
-      long_description=long_description,
-      url="https://forge.ibisc.univ-evry.fr/cthomas/pyits_model_checker",
-      author="Colin Thomas",
-      author_email="cthomas@ens-cachan.fr",
-      classifiers=["Development Status :: 4 - Beta",
-                   "Intended Audience :: Developers",
-                   "Topic :: Scientific/Engineering",
-                   "Programming Language :: Python :: 3",
-                   "Operating System :: OS Independent"],
-      packages=find_packages(where="."),
-      python_requires=">=3.7",
+setup(
+    name="pymc",
+    description=description,
+    long_description=long_description,
+    url="https://forge.ibisc.univ-evry.fr/cthomas/pyits_model_checker",
+    author="Colin Thomas",
+    author_email="cthomas@ens-cachan.fr",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Topic :: Scientific/Engineering",
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    packages=find_packages(where="."),
+    python_requires=">=3.7",
+    install_requires=[
+        "pytl @ git+https://github.com/fpom/pytl.git",
+        "pyddd @ git+https://github.com/fpom/cunf-ptnet-py3.git",
+    ],
 )
